dd0863bea334baa78e5b49bc43885db8e095de3e
jcoo18 <jamiecoombe@gmail.com>
2024-07-29 22:08:09 +1200

    Tested

--- STM32_I2C_Slave_Node_v0.1/STM32_I2C_Slave_Node_v0.1.ino
+++ STM32_I2C_Slave_Node_v0.1/STM32_I2C_Slave_Node_v0.1.ino
@@ -1,7 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+// Timer
+#define TIMER_INTERRUPT_DEBUG         0
+#define _TIMERINTERRUPT_LOGLEVEL_     0
 #include "STM32TimerInterrupt.h"
+
 
 // OLED display dimensions
 #define SCREEN_WIDTH 128
@@ -19,7 +23,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire2, -1);
 #define SLAVE_ADDRESS 0x50
 #define BUFFER_SIZE 8
 // Buffer for I2C communication
-uint8_t i2c_buffer[BUFFER_SIZE] = {0};
+uint8_t i2c_buffer[BUFFER_SIZE] = { 0 };
 
 volatile uint32_t rpm = 0;
 volatile uint32_t speed = 0;
@@ -32,41 +36,40 @@ const int centerX = SCREEN_WIDTH - radius - 5;
 const int centerY = SCREEN_HEIGHT / 2;
 
 // System parameters
-const int maxRPM = 8500; // Max RPM for the engine
+const int maxRPM = 8500;  // Max RPM for the engine
 // Speed sensor parameters
-#define tyreDiameter 18                                                          // Tyre size in inches
-#define tyreWidth 3                                                              // Tyre thickness in inches per side accounting for aspect ratio
-#define tyreCircumferance ((tyreDiameter + (2 * tyreWidth)) * 25.4 * 3.14159265) // Tyre circumference
-#define speedPulsesRev 2                                                         // Number of pulses per revolution
+#define tyreDiameter 18                                                           // Tyre size in inches
+#define tyreWidth 3                                                               // Tyre thickness in inches per side accounting for aspect ratio
+#define tyreCircumferance ((tyreDiameter + (2 * tyreWidth)) * 25.4 * 3.14159265)  // Tyre circumference
+#define speedPulsesRev 2                                                          // Number of pulses per revolution
+
 // Init STM32 timer TIM2
 STM32Timer ITimer1(TIM2);
-const int timInterval = 250; // Timer interval (ms)
+const int timInterval = 250;  // Timer interval (ms)
+
 // Calculation constants
-#define rpmConst 60 * (1000 / timInterval) // Pulses per min for rpm
+#define rpmConst 60 * (1000 / timInterval)  // Pulses per min for rpm
 //                    distance traveled per pulse       # calcs /sec       min hours meters km
-#define speedConst ((tyreCircumferance / speedPulsesRev) * (1000 / timInterval) * 60 * 60 / (1000 * 1000)) // Pulse distance per hour
-#define speedZeroTime ((3 * 1000) / timInterval)  // # seconds * ms / timer interval
+#define speedConst ((tyreCircumferance / speedPulsesRev) * (1000 / timInterval) * 60 * 60 / (1000 * 1000))  // Pulse distance per hour
+#define speedZeroTime ((3 * 1000) / timInterval)                                                            // # seconds * ms / timer interval
+
 // Pin defines
-#define pinRpmPulse PA9
-#define pinSpeedPulse PA10
+#define pinRpmPulse PA11
+#define pinSpeedPulse PA12
 #define ledPin PC13
 
-void setup()
-{
+void setup() {
   // Initialize the serial communication
-  // Serial.begin(115200);
+  Serial.begin(115200);
 
   // Initialize the OLED display on the objects wire port
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    // Serial.println(F("SSD1306 allocation failed"));
+    // for (;;);
   }
 
   // Initialize I2C as slave on the default wire port (PB6=SCL, PB7=SDA)
   Wire.begin(SLAVE_ADDRESS);
-  Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 
   // Pin mode defines
@@ -82,10 +85,8 @@ void setup()
   ITimer1.attachInterruptInterval(timInterval * 1000, CalcTimer);
 }
 
-void loop()
-{
-  if (drawNew)
-  {
+void loop() {
+  if (drawNew) {
     // Update the gauge with the current RPM
     drawRPM(rpm);
     drawSpeed(speed);
@@ -96,86 +97,54 @@ void loop()
 }
 
 // Interrupt service routine for counting RPM pulses
-void pulseISR_RPM()
-{
+void pulseISR_RPM() {
   pulse_rpm++;
   digitalWrite(ledPin, !digitalRead(ledPin));
 }
 
 // Interrupt service routine for counting Speed pulses
-void pulseISR_Speed()
-{
+void pulseISR_Speed() {
   pulse_speed++;
   // digitalWrite(ledPin, !digitalRead(ledPin));
 }
 
 // Timer interrupt service routine for calculating RPM
-void CalcTimer()
-{
+void CalcTimer() {
   // Calc RPM
-  rpm = (pulse_rpm * rpmConst); // Assuming 1 pulse per revolution
-  pulse_rpm = 0;                // Reset pulse count for next measurement
+  rpm = (pulse_rpm * rpmConst);  // Assuming 1 pulse per revolution
+  pulse_rpm = 0;                 // Reset pulse count for next measurement
   // Calc Speed
-  if (pulse_speed > 0)
-  {
+  if (pulse_speed > 0) {
     // If going so slow at least one calculation was missed
-    if (missedPulse_speed > 0)
-    {
+    if (missedPulse_speed > 0) {
       speed = ((pulse_speed * speedConst) / missedPulse_speed);
       missedPulse_speed = 0;
     }
     // Else going faster than the minimum calculation speed
-    else
-    {
+    else {
       speed = (pulse_speed * speedConst);
     }
     pulse_speed = 0;  // Reset pulse count for next measurement
   }
   // if no speed pulse sened then log a missed calculation
-  else
-  {
-    // Incriment the number of calc events missed 
+  else {
+    // Incriment the number of calc events missed
     missedPulse_speed++;
-    
+
     // if too many pulses are missed will reset speed to 0
-    if (missedPulse_speed > speedZeroTime)
-    {
+    if (missedPulse_speed > speedZeroTime) {
       missedPulse_speed = 0;
       speed = 0;
     }
   }
 
-  drawNew = true; // Signal new calculation has been performed and ready to output on the screen
-}
-
-// Function to draw the gauge
-void drawGauge()
-{
-  display.clearDisplay();
-  display.drawCircle(centerX, centerY, radius, SSD1306_WHITE);
-
-  // for (int angle = -90; angle <= 90; angle += 10) {
-  //   float rad = radians(angle);
-  //   int xOuter = centerX + cos(rad) * (radius + 5);
-  //   int yOuter = centerY + sin(rad) * (radius + 5);
-  //   int xInner = centerX + cos(rad) * radius;
-  //   int yInner = centerY + sin(rad) * radius;
-  //   display.drawLine(xOuter, yOuter, xInner, yInner, SSD1306_WHITE);
-  // }
-
-  display.setCursor(0, 0);
-  display.setTextColor(SSD1306_WHITE);
-  display.setTextSize(2);
-  display.print("Speed");
-
-  display.display();
+  drawNew = true;  // Signal new calculation has been performed and ready to output on the screen
 }
 
 // Function to draw the Speed
-void drawSpeed(uint32_t Speed)
-{
+void drawSpeed(uint32_t Speed) {
   // Display the Speed value
-  display.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, SSD1306_BLACK); // Clear the old value
+  display.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, SSD1306_BLACK);  // Clear the old value
   display.setCursor(0, 0);
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(2);
@@ -186,12 +155,11 @@ void drawSpeed(uint32_t Speed)
 }
 
 // Function to draw the RPM
-void drawRPM(uint32_t rpm)
-{
+void drawRPM(uint32_t rpm) {
   //if (rpm > maxRPM)    rpm = maxRPM;
 
   // Display the RPM value
-  display.fillRect(0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, 14, SSD1306_BLACK); // Clear the old value
+  display.fillRect(0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, 14, SSD1306_BLACK);  // Clear the old value
   display.setCursor(0, SCREEN_HEIGHT / 2);
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(2);
@@ -202,31 +170,19 @@ void drawRPM(uint32_t rpm)
 }
 
 // I2C wire events
-void receiveEvent(int howMany)
-{
-  if (howMany == 1)
-  {
-    i2c_buffer[0] = Wire.read();
-  }
-}
+void requestEvent() {
+  // Prepare the response buffer with the rpm then speed data as 32bit numbers in 8 bit packets
+  i2c_buffer[0] = (uint8_t)(rpm & 0xFF);  // LSB
+  i2c_buffer[1] = (uint8_t)((rpm >> 8) & 0xFF);
+  i2c_buffer[2] = (uint8_t)((rpm >> 16) & 0xFF);
+  i2c_buffer[3] = (uint8_t)((rpm >> 24) & 0xFF);  // MSB
+  i2c_buffer[4] = (uint8_t)(speed & 0xFF);        // LSB
+  i2c_buffer[5] = (uint8_t)((speed >> 8) & 0xFF);
+  i2c_buffer[6] = (uint8_t)((speed >> 16) & 0xFF);
+  i2c_buffer[7] = (uint8_t)((speed >> 24) & 0xFF);  // MSB
+  Wire.write(i2c_buffer, BUFFER_SIZE);
 
-void requestEvent()
-{
-  if (i2c_buffer[0] == 0x01)
-  {
-    // Prepare the response buffer with the rpm then speed data as 32bit numbers in 8 bit packets
-    i2c_buffer[1] = (uint8_t)(rpm & 0xFF);        // LSB
-    i2c_buffer[2] = (uint8_t)((rpm >> 8) & 0xFF);
-    i2c_buffer[3] = (uint8_t)((rpm >> 16) & 0xFF);
-    i2c_buffer[4] = (uint8_t)((rpm >> 24) & 0xFF); // MSB
-    i2c_buffer[5] = (uint8_t)(speed & 0xFF);        // LSB
-    i2c_buffer[6] = (uint8_t)((speed >> 8) & 0xFF);
-    i2c_buffer[7] = (uint8_t)((speed >> 16) & 0xFF);
-    i2c_buffer[8] = (uint8_t)((speed >> 24) & 0xFF); // MSB
-    Wire.write(i2c_buffer, BUFFER_SIZE);
-
-    // Clear registers
-    //rpm = 0;
-    //speed = 0;
-  }
+  // Clear registers
+  //rpm = 0;
+  //speed = 0;
 }
--- STM32_I2C_Slave_Sensor_v0.1/STM32_I2C_Slave_Sensor_v0.1.ino
+++ STM32_I2C_Slave_Sensor_v0.1/STM32_I2C_Slave_Sensor_v0.1.ino
@@ -1,67 +1,63 @@
+// Wire Slave Sender
+// by Nicholas Zambetti <http://www.zambetti.com>
+// modified by Jamie Coombe
+
+// Demonstrates use of the Wire library
+// Sends data as an I2C/TWI slave device
+// Refer to the "Wire Master Reader" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
 #include <Wire.h>
 
-
-// I2C address and buffer size
-#define SLAVE_ADDRESS 0x50
-#define BUFFER_SIZE 4
-
 // Pulse counting
-volatile uint16_t low_pulse_count_int0 = 0;
-volatile uint16_t low_pulse_count_int1 = 0;
+volatile uint16_t low_pulse_count_int0 = 8478;
+volatile uint16_t low_pulse_count_int1 = 149;
 
-// Pin defines
-#define pinPulse1 PA9
-#define pinPulse2 PA10
-
+#define BUFFER_SIZE 8
 // Buffer for I2C communication
 uint8_t i2c_buffer[BUFFER_SIZE] = {0};
 
-
 void setup() {
-  // Initialize Serial Monitor for debugging
-  //Serial.begin(9600);
-  
-  // Initialize I2C as slave
-  Wire.begin(SLAVE_ADDRESS);
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
-  
-  // Initialize external interrupts for pulse counting
-  pinMode(2, INPUT_PULLUP);  // INT0
-  pinMode(3, INPUT_PULLUP);  // INT1
-  attachInterrupt(digitalPinToInterrupt(pinRpmPulse), countPulseInt0, FALLING);
-  attachInterrupt(digitalPinToInterrupt(pinSpeedPulse), countPulseInt1, FALLING);
+  Serial.begin(115200);
+  Wire.begin(0x50);              // join i2c bus with address #2
+  Wire.onRequest(requestEvent);  // register event
+
+  Serial.println("Setup n ready");
 }
 
 void loop() {
-  // Main loop can be used for other tasks
+  delay(100);
 }
 
-void countPulseInt0() {
-  low_pulse_count_int0++;
-}
-
-void countPulseInt1() {
-  low_pulse_count_int1++;
-}
-
-void receiveEvent(int howMany) {
-  if (howMany == 1) {
-    i2c_buffer[0] = Wire.read();
-  }
-}
-
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
 void requestEvent() {
-  if (i2c_buffer[0] == 0x01) {
-    // Prepare the response buffer with the pulse count data
-    i2c_buffer[1] = (uint8_t)(low_pulse_count_int0 & 0xFF); // LSB
-    i2c_buffer[2] = (uint8_t)((low_pulse_count_int0 >> 8) & 0xFF); // MSB
-    i2c_buffer[3] = (uint8_t)(low_pulse_count_int1 & 0xFF); // LSB
-    i2c_buffer[4] = (uint8_t)((low_pulse_count_int1 >> 8) & 0xFF); // MSB
-    Wire.write(i2c_buffer, BUFFER_SIZE);
+  //Wire.write("hello "); // respond with message of 6 bytes
+  // as expected by master
+  i2c_buffer[0] = (uint8_t)(low_pulse_count_int0 & 0xFF);  // LSB
+  i2c_buffer[1] = (uint8_t)((low_pulse_count_int0 >> 8) & 0xFF);
+  i2c_buffer[2] = (uint8_t)((low_pulse_count_int0 >> 16) & 0xFF);
+  i2c_buffer[3] = (uint8_t)((low_pulse_count_int0 >> 24) & 0xFF);  // MSB
+  i2c_buffer[4] = (uint8_t)(low_pulse_count_int1 & 0xFF);          // LSB
+  i2c_buffer[5] = (uint8_t)((low_pulse_count_int1 >> 8) & 0xFF);
+  i2c_buffer[6] = (uint8_t)((low_pulse_count_int1 >> 16) & 0xFF);
+  i2c_buffer[7] = (uint8_t)((low_pulse_count_int1 >> 24) & 0xFF);  // MSB
+  Wire.write(i2c_buffer, BUFFER_SIZE);
+  
+  Serial.print("int0: ");
+  Serial.print(low_pulse_count_int0);
+  Serial.print(", int1: ");
+  Serial.println(low_pulse_count_int1);
 
-    // Clear registers
-    low_pulse_count_int0 = 0;
-    low_pulse_count_int1 = 0;
+  Serial.print("Buffer: ");
+  for(uint8_t i=0; i<BUFFER_SIZE; i++)
+  {
+    Serial.print(i2c_buffer[i]);
+    Serial.print(" ");
   }
+  Serial.println();
 }
